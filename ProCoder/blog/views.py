1c3b50bebaebcbf8839fdde5cfbf43ab515a18ed
Hasib <hasibul.syl@gmail.com>
2021-06-13 20:47:53 +0600

    1st commit

---
+++ ProCoder/blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+
+def blogHome(request):
+    return HttpResponse("This is blogHome. We will keep all the blog posts here")
+
+def blogPost(request, slug):
+    return HttpResponse(f"This is blogPost : {slug}")
+
